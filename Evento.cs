f88893d173fab59a80a7b54327b1330e914f2b10
AlessP93 <alespiscino@libero.it>
2022-09-26 11:08:58 +0200

    correzione data

--- Evento.cs
+++ Evento.cs
@@ -1,12 +1,12 @@
 ﻿//creare una classe Evento che abbia i seguenti attributi: 
 public class Evento
 {
-    public int Titolo { get; set; }
-    public DateOnly Data { get; set; }
+    public string Titolo { get; set; }
+    public DateTime Data { get; }
     public double MaxCapienza { get; }
     public int PostiPrenotati { get; }
 
-    public Evento(int titolo, DateOnly data, double maxCapienza, int postiPrenotati)
+    public Evento(string titolo, DateTime data, double maxCapienza, int postiPrenotati)
     {
         Titolo = titolo;
         Data = data;
@@ -15,12 +15,13 @@ public class Evento
     }
 
     //controlli
-    public void DataNonPassata()
+    public void DataPassata()
     {
         DateTime DataOggi = DateTime.Now;
         if (Data < DataOggi)
         {
-            Console.WriteLine("Data non disponibile");
+            Console.WriteLine("La data è già passata");
         }
     }
+    public void 
 }
