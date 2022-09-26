0a14f9c1fc1e279d1a5b05f274daac5fb328bddf
AlessP93 <alespiscino@libero.it>
2022-09-26 10:56:18 +0200

    Aggiungere i file di progetto.

---
+++ Evento.cs
@@ -0,0 +1,26 @@
+﻿//creare una classe Evento che abbia i seguenti attributi: 
+public class Evento
+{
+    public int Titolo { get; set; }
+    public DateOnly Data { get; set; }
+    public double MaxCapienza { get; }
+    public int PostiPrenotati { get; }
+
+    public Evento(int titolo, DateOnly data, double maxCapienza, int postiPrenotati)
+    {
+        Titolo = titolo;
+        Data = data;
+        MaxCapienza = maxCapienza;
+        PostiPrenotati = postiPrenotati;
+    }
+
+    //controlli
+    public void DataNonPassata()
+    {
+        DateTime DataOggi = DateTime.Now;
+        if (Data < DataOggi)
+        {
+            Console.WriteLine("Data non disponibile");
+        }
+    }
+}
